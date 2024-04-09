128a9bebaa1afe56a97170ef5eb93e8083c97d69
Valeriy <valerka.kondratenkov@mail.ru>
2024-04-10 02:33:24 +0500

    added encryption classes

---
+++ RefreshCourseClient/RefreshCourseClient/Models/AuthResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefreshCourseClient.Models
+{
+    internal class AuthResponseModel
+    {
+        public string? Token { get; set; }
+        public string? PublicKey { get; set; }
+    }
+}
---
+++ RefreshCourseClient/RefreshCourseClient/Models/RecordModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefreshCourseClient.Models
+{
+    internal class RecordModel
+    {
+        public int Id { get; set; }
+        public string? GroupName { get; set; }
+        public string? SubjectName { get; set; }
+        public string? LessonType { get; set; }
+        public int HoursCount { get; set; }
+
+        public double PayHour { get; set; }
+        public double Money { get; set; }
+    }
+}
