f99f7108a0d8150291adf5d3817a14c0ad9d71c6
Valeriy <valerka.kondratenkov@mail.ru>
2024-04-09 21:46:57 +0500

    added messaging methods

--- RefreshCourseClient/RefreshCourseClient/Data/Services/MessengerService.cs
+++ RefreshCourseClient/RefreshCourseClient/Data/Services/MessengerService.cs
@@ -5,6 +5,8 @@ namespace RefreshCourseClient.Data.Services
     internal interface IMessengerService
     {
         static abstract void ShowErrorMessageBox(string message);
+        static abstract void ShowInfoMessageBox(string message);
+        static abstract void ShowWarningMessageBox(string title, string message);
     }
 
     internal class MessengerService : IMessengerService
@@ -13,5 +15,16 @@ namespace RefreshCourseClient.Data.Services
         {
             MessageBox.Show(message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
         }
+
+        public static void ShowInfoMessageBox(string message)
+        {
+            MessageBox.Show(message, "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public static void ShowWarningMessageBox(string title, string message)
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+        }
+
     }
 }
